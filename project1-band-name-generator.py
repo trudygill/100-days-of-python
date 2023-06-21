dedc554219693285910bd2d4d671c115b03315ff
Trudy Gill <trudymgill@gmail.com>
2023-06-21 19:22:52 -0400

    Amended the code to be more readable

--- project1-band-name-generator.py
+++ project1-band-name-generator.py
@@ -1,8 +1,11 @@
 #1. Create a greeting for the program
 print("Hello! Welcome to the band name generator! 🎵")
+
 #2. Ask the user for the city that they grew up in
 user_city = input("Which city did you grow up in?\n")
+
 #3. Ask the user for the last thing they ate
 last_meal = input("What is the last thing you ate?\n")
+
 #4. Combine the name of their city and pet and show their band name.
 print(f"Your band name could be: {user_city} {last_meal}")
